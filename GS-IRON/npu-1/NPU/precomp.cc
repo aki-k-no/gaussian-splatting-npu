f2bd462c79f73866aecdf55b5a063b2451ca686f
aki-k-no <okkundoriit2@gmail.com>
2025-12-01 23:50:51 +0900

    at least stable one

--- GS-IRON/npu-1/NPU/precomp.cc
+++ GS-IRON/npu-1/NPU/precomp.cc
@@ -24,6 +24,8 @@ const int M=4;
 const int K=8;
 const int N=4;
 
+// projection from world to view space
+
 // note that size of proj mat is always 4x4, gaussian is 4xN
 template <const int GAUSSIAN_SIZE>
 void proj_to_view_space(float *restrict proj_mat, float *restrict gaussians, float *restrict output) {
@@ -40,9 +42,50 @@ void proj_to_view_space(float *restrict proj_mat, float *restrict gaussians, flo
     
     aie::accum<accfloat, 16> acc = aie::zeros<accfloat,16>();
     
-    event0();
     AIE_PREPARE_FOR_PIPELINING
-    AIE_LOOP_RANGE(64, 64)
+    AIE_LOOP_RANGE(GAUSSIAN_SIZE / 4, GAUSSIAN_SIZE / 4)
+    // compute over all elements
+    for (size_t i = 0; i < GAUSSIAN_SIZE / 4; i += 1) {
+        event0();
+        //load elements
+        aie::vector<float, 16> y=aie::load_v<16>(gaussians + i * 16);
+        aie::vector<float, 16> y0 = ::extract_v4float_broadcast_to_v16float(y, 0);
+        aie::vector<float, 16> y1 = ::extract_v4float_broadcast_to_v16float(y, 1);
+        aie::vector<float, 16> y2 = ::extract_v4float_broadcast_to_v16float(y, 2);
+        aie::vector<float, 16> y3 = ::extract_v4float_broadcast_to_v16float(y, 3);
+        
+        event1();
+        acc =  aie::zeros<accfloat,16>();
+
+        acc = ::mul_elem_16(x0, y0);
+        acc = ::mac_elem_16(x1, y1, acc);
+        acc = ::mac_elem_16(x2, y2, acc);
+        acc = ::mac_elem_16(x3, y3, acc);
+        
+        // store data
+        aie::store_v(output + i * 16, acc.to_vector<float>());
+        
+    }
+
+    return;
+}
+
+// projection from world to camera
+template <const int GAUSSIAN_SIZE>
+void get_camera_pos(float* restrict camera_mat, float *restrict gaussians, float *restrict output){
+    // load input data
+    aie::vector<float, 16> va = ::shuffle(aie::load_v<16>(camera_mat), T32_4x4);
+
+    aie::vector<float, 16> x0 = ::shuffle(::extract_v4float_broadcast_to_v16float(va, 0), T32_4x4);
+    aie::vector<float, 16> x1 = ::shuffle(::extract_v4float_broadcast_to_v16float(va, 1), T32_4x4);
+    aie::vector<float, 16> x2 = ::shuffle(::extract_v4float_broadcast_to_v16float(va, 2), T32_4x4);
+    aie::vector<float, 16> x3 = ::shuffle(::extract_v4float_broadcast_to_v16float(va, 3), T32_4x4);
+    
+    
+    aie::accum<accfloat, 16> acc = aie::zeros<accfloat,16>();
+    
+    AIE_PREPARE_FOR_PIPELINING
+    AIE_LOOP_RANGE(GAUSSIAN_SIZE / 4, GAUSSIAN_SIZE / 4)
     // compute over all elements
     for (size_t i = 0; i < GAUSSIAN_SIZE / 4; i += 1) {
         //load elements
@@ -57,26 +100,19 @@ void proj_to_view_space(float *restrict proj_mat, float *restrict gaussians, flo
         acc = ::mul_elem_16(x0, y0);
         acc = ::mac_elem_16(x1, y1, acc);
         acc = ::mac_elem_16(x2, y2, acc);
-        
         acc = ::mac_elem_16(x3, y3, acc);
         
-        
-        
-
         // store data
         aie::store_v(output + i * 16, acc.to_vector<float>());
         
     }
 
-
-
-    
-    event1();
     return;
+
 }
 
 extern "C" {
 
-void f32_proj_to_view_space(float *proj_in, float *gaussian_in, float *out) { proj_to_view_space<256>(proj_in, gaussian_in, out); }
+void f32_proj_to_view_space(float *proj_in, float *gaussian_in, float *out) { proj_to_view_space<64>(proj_in, gaussian_in, out); }
 
 } // extern "C"
--- GS-IRON/npu-1/NPU/precomp.py
+++ GS-IRON/npu-1/NPU/precomp.py
@@ -22,6 +22,7 @@ from aie.dialects.aie import *
 from aie.dialects.aiex import *
 from aie.extras.context import mlir_mod_ctx
 from aie.helpers.dialects.ext.scf import _for as range_
+import aie.utils.trace as trace_utils
 
 
 
@@ -36,49 +37,68 @@ def precomp(dev):
         
         # Define tensor types
         line_size = 256
-        proj_ty = np.ndarray[(4*4,), np.dtype[xfr_dtype]]
-        gaussian_ty = np.ndarray[(4*line_size,), np.dtype[xfr_dtype]]
+        sub_tiles = 4
+        world_to_view_size = 4 * 4
+        w2v_ty = np.ndarray[(world_to_view_size,), np.dtype[xfr_dtype]]
+        gaussian_ty = np.ndarray[(4*line_size // sub_tiles,), np.dtype[xfr_dtype]]
+
+        essentials_ty = np.ndarray[(world_to_view_size,), np.dtype[xfr_dtype]]
 
         # AIE Core Function declarations
-        proj_func = external_func(
-            "f32_proj_to_view_space", inputs=[proj_ty, gaussian_ty, gaussian_ty]
+        w2v_func = external_func(
+            "f32_proj_to_view_space", inputs=[w2v_ty, gaussian_ty, gaussian_ty]
         )
 
         # Tile declarations
         ShimTile0 = tile(0, 0)
-        ComputeTileProj = tile(0, 2)
+        MemTile0 = tile(0, 1)
+        ComputeTileV2w = tile(0, 2)
 
         # AIE-array data movement with object fifos
-        of_proj = object_fifo("proj", ShimTile0, ComputeTileProj, 2, proj_ty)
-        of_gaussian = object_fifo("gaussian", ShimTile0, ComputeTileProj, 2, gaussian_ty)
-        of_out = object_fifo("out", ComputeTileProj, ShimTile0, 2, gaussian_ty)
+        of_essentials = object_fifo("essentials", ShimTile0, MemTile0, 2, essentials_ty)
+        of_w2v = object_fifo("w2v", MemTile0, ComputeTileV2w, 2, w2v_ty)
+        object_fifo_link(of_essentials, of_w2v)
+
+        of_gaussian = object_fifo("gaussian", ShimTile0, ComputeTileV2w, 2, gaussian_ty)
+        of_out = object_fifo("out", ComputeTileV2w, ShimTile0, 2, gaussian_ty)
 
 
         # Compute tile for Projection Mat
-        @core(ComputeTileProj, "precomp.o")
+        @core(ComputeTileV2w, "precomp.o")
         def core_body():
             for _ in range_(0xFFFFFFFF):
-                elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
-                elemIn1 = of_proj.acquire(ObjectFifoPort.Consume, 1)
-                elemIn2 = of_gaussian.acquire(ObjectFifoPort.Consume, 1)
-                proj_func(elemIn1, elemIn2, elemOut)
-                of_proj.release(ObjectFifoPort.Consume, 1)
-                of_gaussian.release(ObjectFifoPort.Consume, 1)
-                of_out.release(ObjectFifoPort.Produce,1)
+                elemIn1 = of_w2v.acquire(ObjectFifoPort.Consume, 1)
+                for _ in range_(4):
+                    elemOut = of_out.acquire(ObjectFifoPort.Produce, 1)
+                    elemIn2 = of_gaussian.acquire(ObjectFifoPort.Consume, 1)
+                    w2v_func(elemIn1, elemIn2, elemOut)
+                    of_gaussian.release(ObjectFifoPort.Consume, 1)
+                    of_out.release(ObjectFifoPort.Produce,1)
+                of_w2v.release(ObjectFifoPort.Consume, 1)
+                
+        tiles_to_trace = [ComputeTileV2w, ShimTile0]
+        if trace_size > 0:
+            trace_utils.configure_packet_tracing_flow(tiles_to_trace, ShimTile0)
 
 
         # To/from AIE-array data movement
-        @runtime_sequence(proj_ty, gaussian_ty, gaussian_ty)
+        @runtime_sequence(essentials_ty, gaussian_ty, gaussian_ty)
         def sequence(A, B, C):
-            proj_task = shim_dma_single_bd_task(of_proj, A, sizes=[1, 1, 1, 16])
+            if trace_size > 0:
+                trace_utils.configure_packet_tracing_aie2(
+                    tiles_to_trace=tiles_to_trace,
+                    shim=ShimTile0,
+                    trace_size=trace_size,
+                )
+            import_task = shim_dma_single_bd_task(of_essentials, A, sizes=[1, 1, 1, 16])
             gaussian_task = shim_dma_single_bd_task(of_gaussian, B, sizes=[1, 1, 1, line_size * 4])
             out_task = shim_dma_single_bd_task(
                 of_out, C, sizes=[1, 1, 1, line_size * 4], issue_token=True
             )
 
-            dma_start_task(proj_task, gaussian_task, out_task)
+            dma_start_task(import_task, gaussian_task, out_task)
             dma_await_task(out_task)
-            dma_free_task(proj_task)
+            dma_free_task(import_task)
             dma_free_task(gaussian_task)
     
 
